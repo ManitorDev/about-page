3f42d3376dfc9390cae6d3ee5a14cf8abe262034
Mani <mani.developer4@gmail.com>
2024-07-07 00:00:22 +0330

    complete

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// const circleRT = document.getElementById("circle-rt");
+// let cy = 100;
+// let calcValue = 1;
+
+// const circleRTcyAnimation = () => {
+//   circleRT.setAttribute("cy", cy);
+//   cy += calcValue;
+//   if (cy >= 120) {
+//     calcValue = -1;
+//   } else if (cy <= 100) {
+//     calcValue = 1;
+//   }
+//   requestAnimationFrame(circleRTcyAnimation);
+// };
+
+// circleRTcyAnimation();
